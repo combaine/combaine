29f2d37eea8c2e60d5d9cf1827c168f63c2fd057
noxiouz <noxiouz@yandex.ru>
2013-11-08 04:17:52 +0400

    Add application for handling data by user-defined parser

---
+++ core/parsingApp.py
@@ -0,0 +1,66 @@
+#!/usr/bin/env python
+import os
+import imp
+
+import msgpack
+
+from cocaine.worker import Worker
+from cocaine.logging import Logger
+
+Log = Logger()
+
+
+PATH = '/usr/lib/yandex/combaine/parsers'
+
+
+def _isPlugin(candidate):
+    name, extension = os.path.splitext(candidate)
+    if name != "__init__" and extension in (".py", ".pyc", ".pyo"):
+        return True
+    else:
+        return False
+
+
+def plugin_import():
+    modules = set(map(lambda x: x.split('.')[0], filter(_isPlugin,
+                                                        os.listdir(PATH))))
+    all_parser_functions = {}
+    for module in modules:
+        try:
+            fp, path, descr = imp.find_module(module, [PATH])
+        except ImportError:
+            continue
+        else:
+            try:
+                _temp = imp.load_module("temp", fp, path, descr)
+                for item in filter(lambda x: not x.startswith("__"),
+                                   dir(_temp)):
+                    candidate = getattr(_temp, item)
+                    if callable(candidate):
+                        all_parser_functions[item] = candidate
+            except ImportError:
+                pass
+            except Exception:
+                pass
+            finally:
+                if fp:
+                    fp.close()
+    return all_parser_functions
+
+
+def parse(request, response):
+    inc = yield request.read()
+    name, data = msgpack.unpackb(inc)
+    Log.info(name)
+    func = plugin_import().get(name)
+    if func is None:
+        response.write("Missing function %s" % name)
+        response.close()
+        raise StopIteration
+    response.write(func(data))
+    response.close()
+
+
+if __name__ == "__main__":
+    W = Worker()
+    W.run({"parse": parse})
