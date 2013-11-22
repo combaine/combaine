5af46ba5c8d36daf4776dd1fb1e8802600522663
noxiouz <noxiouz@yandex.ru>
2013-11-22 11:28:34 +0400

    Add presetting package

--- deploy_scripts/deploy_parsing-core.sh
+++ deploy_scripts/deploy_parsing-core.sh
@@ -8,3 +8,4 @@ rm ./$PACKAGE || true
 tar -czf ./$PACKAGE ./*
 
 cocaine-tool app upload --name $APPNAME --package=$PACKAGE --manifest=$MANIFEST
+cocaine-tool runlist add-app --name combaine --app $APPNAME --profile default
---
+++ presetting/presetting.py
@@ -0,0 +1,35 @@
+#!/usr/bin/env python
+import os
+
+from cocaine.tools.actions import runlist, profile
+from cocaine.services import Service
+from cocaine.exceptions import ChokeEvent
+
+COMBAINE_RUNLIST = "combaine"
+
+try:
+    s = Service("storage")
+    if "default" not in profile.List(s).execute().get():
+        try:
+            print("Create empty profile")
+            profile.Upload(s, "default", os.path.dirname(__file__)+"/profile.json").execute().get()
+        except ChokeEvent:
+            pass
+    if COMBAINE_RUNLIST not in runlist.List(s).execute().get():
+        try:
+            print("Create empty runlist")
+            runlist.Upload(s, COMBAINE_RUNLIST, os.path.dirname(__file__)+"/runlist.json").execute().get()
+        except ChokeEvent:
+            pass
+except Exception as err:
+    print('%s' % repr(err))
+    exit(1)
+
+
+
+
+
+
+
+
+
