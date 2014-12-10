9c9e18f3344506010d19c213d2c307e503e02ccc
Anton Tiurin <noxiouz@yandex.ru>
2014-12-10 23:36:16 +0300

    [Client] Move hostfetcher out of client code

--- combainer/context.go
+++ combainer/context.go
@@ -4,6 +4,9 @@ import (
 	"github.com/noxiouz/Combaine/common/cache"
 )
 
+type CloudHostsDelegate func() ([]string, error)
+
 type Context struct {
 	Cache cache.Cache
+	Hosts CloudHostsDelegate
 }
