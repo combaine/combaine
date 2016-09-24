a60869a596ccc764931f09be68fce04bfd1e9d03
Sergey Kacheev <s.kacheev@gmail.com>
2016-09-24 16:02:06 +0300

    (senders) graphite dial timeout at 1500ms

--- senders/graphite/graphite.go
+++ senders/graphite/graphite.go
@@ -14,16 +14,14 @@ import (
 )
 
 func formatSubgroup(input string) string {
-	return strings.Replace(
-		strings.Replace(input, ".", "_", -1),
-		"-", "_", -1)
+	return strings.Replace(strings.Replace(input, ".", "_", -1), "-", "_", -1)
 }
 
 const (
 	onePointFormat = "%s.combaine.%s.%s %s %d\n"
 
-	connectionTimeout  = 900      //msec
-	connectionEndpoint = ":42000" //msec
+	connectionTimeout  = 1500 //msec
+	connectionEndpoint = ":42000"
 )
 
 type GraphiteSender interface {
@@ -187,11 +185,15 @@ func (g *graphiteClient) Send(data tasks.DataType, timestamp uint64) (err error)
 	}
 
 	sock, err := net.DialTimeout("tcp", connectionEndpoint, time.Microsecond*connectionTimeout)
+	defer func() {
+		if sock != nil {
+			sock.Close()
+		}
+	}()
 	if err != nil {
 		logger.Errf("Unable to connect to daemon %s: %s", connectionEndpoint, err)
 		return
 	}
-	defer sock.Close()
 	return g.sendInternal(&data, timestamp, sock)
 }
 
