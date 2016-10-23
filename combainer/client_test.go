9e24ada522f0f8ca1898ad7bad2dcd9ab606062d
Anton Tyurin <noxiouz@yandex.ru>
2016-10-23 22:54:55 +0300

    grpc: drop deprecated tests

--- combainer/client_test.go
+++ combainer/client_test.go
@@ -1,11 +1,8 @@
 package combainer
 
 import (
-	"context"
 	"fmt"
-	"log"
 	"testing"
-	"time"
 
 	"github.com/Sirupsen/logrus"
 	"github.com/combaine/combaine/common/configs"
@@ -20,67 +17,11 @@ func TestNewClient(t *testing.T) {
 
 	context := &Context{
 		Cache:  nil,
-		Serf:   nil,
 		Logger: logrus.StandardLogger(),
 	}
 
 	c, err := NewClient(context, repo)
 	assert.Nil(t, err, fmt.Sprintf("Unable to create client %s", err))
 	assert.NotEmpty(t, c.ID)
-}
-
-func TestGetRandomHost(t *testing.T) {
-	cases := []struct {
-		hosts []string
-		empty bool
-	}{
-		{[]string{}, true},
-		{[]string{"host1", "host2", "host3", "host4", "host5", "hosts6"}, false},
-	}
-
-	for _, c := range cases {
-		if c.empty {
-			assert.Equal(t, "", getRandomHost(c.hosts))
-		} else {
-			cur, prev := "", ""
-			random := 0
-			for i := 0; i < 10; i++ {
-				cur = getRandomHost(c.hosts)
-				assert.NotEqual(t, "", cur)
-				if i != 0 && cur != prev {
-					random++
-				}
-				prev = cur
-			}
-			if random < 5 {
-				log.Fatal("random very predictable")
-			}
-			log.Printf("Got %d random hosts", random)
-		}
-	}
-}
-
-func TestDialContext(t *testing.T) {
-	cases := []struct {
-		hosts    []string
-		expected string
-		empty    bool
-	}{
-		{[]string{}, "empty list of hosts", true},
-		{[]string{"host1", "host2", "host3", "host4", "host5", "hosts6"},
-			"context deadline exceeded", false},
-	}
-
-	for _, c := range cases {
-		ctx, cancel := context.WithTimeout(context.TODO(), time.Millisecond)
-		gClient, err := dialContext(ctx, c.hosts)
-		if c.empty {
-			assert.Error(t, err)
-			assert.Contains(t, err.Error(), c.expected)
-		} else {
-			assert.Nil(t, gClient)
-			assert.Contains(t, err.Error(), c.expected)
-		}
-		cancel()
-	}
+	c.Close()
 }
