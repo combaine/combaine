53146a70f38d0a8dc315f420603e40876087cd4e
Sergey Kacheev <s.kacheev@gmail.com>
2016-12-14 21:52:41 +0700

    agave: space manipulation

--- senders/agave/agave_test.go
+++ senders/agave/agave_test.go
@@ -39,35 +39,19 @@ func TestSend(t *testing.T) {
 	assert.NoError(t, err)
 
 	data := []tasks.AggregationResult{
-		{
-			Tags:   map[string]string{"type": "host", "name": "host1", "metahost": "host1", "aggregate": "20x"},
-			Result: 2000,
-		},
-		{
-			Tags: map[string]string{"type": "datacenter", "name": "DC1", "metahost": "host3", "aggregate": "20x"},
+		{Tags: map[string]string{"type": "host", "name": "host1", "metahost": "host1", "aggregate": "20x"},
+			Result: 2000},
+		{Tags: map[string]string{"type": "datacenter", "name": "DC1", "metahost": "host3", "aggregate": "20x"},
 			Result: map[string]interface{}{
 				"MAP1": []interface{}{201, 301, 401},
 				"MAP2": []interface{}{202, 302, 402},
-			},
-		},
-		{
-			Tags: map[string]string{"type": "host", "name": "host4", "metahost": "host4", "aggregate": "20x"},
-			Result: map[string]interface{}{
-				"MP1": 1000,
-				"MP2": 1002,
-			},
-		},
-		{
-			Tags:   map[string]string{"type": "metahost", "name": "host2", "metahost": "host2", "aggregate": "30x"},
-			Result: []int{20, 30, 40},
-		},
-		{
-			Tags: map[string]string{"type": "metahost", "name": "host4", "metahost": "host4", "aggregate": "30x"},
-			Result: map[string]interface{}{
-				"MP1": 1000,
-				"MP2": 1002,
-			},
-		},
+			}},
+		{Tags: map[string]string{"type": "host", "name": "host4", "metahost": "host4", "aggregate": "20x"},
+			Result: map[string]interface{}{"MP1": 1000, "MP2": 1002}},
+		{Tags: map[string]string{"type": "metahost", "name": "host2", "metahost": "host2", "aggregate": "30x"},
+			Result: []int{20, 30, 40}},
+		{Tags: map[string]string{"type": "metahost", "name": "host4", "metahost": "host4", "aggregate": "30x"},
+			Result: map[string]interface{}{"MP1": 1000, "MP2": 1002}},
 	}
 
 	expected := map[string][]string{
