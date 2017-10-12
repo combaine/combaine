3ce0a2dbc3c013823d52af93885b0339de8549d4
Sergey Kacheev <s.kacheev@gmail.com>
2017-10-13 00:18:27 +0700

    custom: refactor

--- plugins/aggregators/custom.py
+++ plugins/aggregators/custom.py
@@ -15,23 +15,10 @@ from cocaine.logging import Logger
 from cocaine.logging.hanlders import CocaineHandler
 # pylint: enable=import-error
 
-LOG = Logger()
-LOG.error("INITIALIZE")
-
-LOG = logging.getLogger("combaine")
-LOG.setLevel(logging.DEBUG)
-CLH = CocaineHandler()
-CLH.setFormatter(logging.Formatter("%(tid)s %(message)s"))
-CLH.setLevel(logging.DEBUG)
-LOG.addHandler(CLH)
-
+Logger().info("Initialize custom.py")
 PATH = os.environ.get('PLUGINS_PATH', '/usr/lib/yandex/combaine/custom')
 sys.path.insert(0, PATH)
-
 EXTS = [ext for (ext, _, _) in imp.get_suffixes()]
-
-RELOAD_PLUGIN_INTERVAL = 180  # seconds
-LAST_PLUGIN_LOAD_TIME = time() - (RELOAD_PLUGIN_INTERVAL + 1) # trigger update
 
 
 def _is_plugin(candidate):
@@ -43,117 +30,128 @@ def _is_candidate(name):
     return not name.startswith("_") and name[0].isupper()
 
 
-def plugin_import():
-    """
-    It tries to import the extensions of a custom aggregator
-    in the private namespace and select all the names starting
-    with an uppercase letter
-    """
+class Custom(object):
+    """Combaine custom plugin loader"""
 
-    if LAST_PLUGIN_LOAD_TIME + RELOAD_PLUGIN_INTERVAL > time():
-        return plugin_import.all_custom_parsers
+    def __init__(self):
+        self.log = logging.getLogger("combaine")
+        self.log.setLevel(logging.DEBUG)
+        clh = CocaineHandler()
+        clh.setFormatter(logging.Formatter("%(tid)s %(message)s"))
+        clh.setLevel(logging.DEBUG)
+        self.log.addHandler(clh)
 
-    global LAST_PLUGIN_LOAD_TIME # pylint: disable=global-statement
-    LAST_PLUGIN_LOAD_TIME = time()
+        self.reload_interval = 180  # seconds
+        self.last_load_time = time()
+        self.all_custom_parsers = {}
+        self.plugin_import()
 
-    parsers = {}
-    logger = logging.LoggerAdapter(LOG, {"tid": "plugin_import"})
-    modules = set(os.path.splitext(c)[0] for c in os.listdir(PATH) if _is_plugin(c))
-    for module in modules:
+
+    def plugin_import(self):
+        """
+        It tries to import the extensions of a custom aggregator
+        in the private namespace and select all the names starting
+        with an uppercase letter
+        """
+
+        if self.last_load_time + self.reload_interval > time():
+            return self.all_custom_parsers
+        self.last_load_time = time()
+
+        parsers = {}
+        logger = logging.LoggerAdapter(self.log, {"tid": "plugin_import"})
+        modules = set(
+            os.path.splitext(c)[0] for c in os.listdir(PATH) if _is_plugin(c)
+        )
+        for module in modules:
+            try:
+                mfp, path, descr = imp.find_module(module, [PATH])
+            except ImportError as err:
+                logger.error("ImportError. Module: %s %s" % (module, repr(err)))
+                continue
+
+            try:
+                _temp = imp.load_module(module, mfp, path, descr)
+                for item in (x for x in dir(_temp) if _is_candidate(x)):
+                    candidate = getattr(_temp, item)
+                    if callable(candidate):
+                        parsers[item] = candidate
+            except ImportError as err:
+                logger.error("ImportError. Module: %s %s" % (module, repr(err)))
+            except Exception as err:  # pylint: disable=broad-except
+                logger.error("Exception. Module: %s %s" % (module, repr(err)))
+            finally:
+                if mfp:
+                    mfp.close()
+        logger.debug("%s are available custom plugin for parsing" % parsers.keys())
+        self.all_custom_parsers = parsers
+
+
+    def aggregate_host(self, request, response):
+        """
+        Gets the result of a single host,
+        performs parsing and their aggregation
+        """
+        logger = self.log
         try:
-            mfp, path, descr = imp.find_module(module, [PATH])
-        except ImportError as err:
-            logger.error("ImportError. Module: %s %s" % (module, repr(err)))
-            continue
+            raw = yield request.read()
+            task = msgpack.unpackb(raw)
+            logger = logging.LoggerAdapter(self.log, {"tid": task['Id']})
 
-        try:
-            _temp = imp.load_module(module, mfp, path, descr)
-            for item in (x for x in dir(_temp) if _is_candidate(x)):
-                candidate = getattr(_temp, item)
-                if callable(candidate):
-                    parsers[item] = candidate
-        except ImportError as err:
-            logger.error("ImportError. Module: %s %s" % (module, repr(err)))
+            payload = task['Data']
+            cfg = task['Config']
+            klass_name = cfg['class']
+            cfg['logger'] = logger
+
+            self.plugin_import()
+            klass = self.all_custom_parsers[klass_name]
+            prevtime, currtime = task["PrevTime"], task["CurrTime"]
+            result = klass(cfg).aggregate_host(payload, prevtime, currtime)
+
+            response.write(msgpack.packb(result))
+        except KeyError:
+            response.error(-100, "There's no class named %s" % klass_name)
+            logger.error("class %s is absent", klass_name)
         except Exception as err:  # pylint: disable=broad-except
-            logger.error("Exception. Module: %s %s" % (module, repr(err)))
+            response.error(-3, "Exception during handling %s" % repr(err))
+            logger.error("Error %s", err)
         finally:
-            if mfp:
-                mfp.close()
-    logger.debug("%s are available custom plugin for parsing" % parsers.keys())
-    plugin_import.all_custom_parsers = parsers
-    return parsers
+            response.close()
 
 
-def aggregate_host(request, response):
-    """
-    Gets the result of a single host,
-    performs parsing and their aggregation
-    """
-    try:
-        raw = yield request.read()
-        task = msgpack.unpackb(raw)
-        logger = logging.LoggerAdapter(LOG, {"tid": task['Id']})
+    def aggregate_group(self, request, response):
+        """
+        Receives a list of results from the aggregate_host,
+        and performs aggregation by group
+        """
+        logger = self.log
+        try:
+            raw = yield request.read()
+            task = msgpack.unpackb(raw)
+            logger = logging.LoggerAdapter(self.log, {'tid': task['Id']})
+            payload = map(msgpack.unpackb, task['Data'])
+            cfg = task['Config']
+            klass_name = cfg['class']
+            cfg['logger'] = logger
 
-        payload = task['Data']
-        cfg = task['Config']
-        klass_name = cfg['class']
-        cfg['logger'] = logger
+            self.plugin_import()
+            klass = self.all_custom_parsers[klass_name]
+            result = klass(cfg).aggregate_group(payload)
 
-        result = _aggregate_host(klass_name, payload, cfg, task)
-        response.write(msgpack.packb(result))
-    except KeyError:
-        response.error(-100, "There's no class named %s" % klass_name)
-        logger.error("class %s is absent", klass_name)
-    except Exception as err:  # pylint: disable=broad-except
-        response.error(-3, "Exception during handling %s" % repr(err))
-        logger.error("Error %s", err)
-    finally:
-        response.close()
-
-
-def aggregate_group(request, response):
-    """
-    Receives a list of results from the aggregate_host,
-    and performs aggregation by group
-    """
-    logger = LOG
-    try:
-        raw = yield request.read()
-        task = msgpack.unpackb(raw)
-        logger = logging.LoggerAdapter(LOG, {'tid': task['Id']})
-        payload = map(msgpack.unpackb, task['Data'])
-        cfg = task['Config']
-        klass_name = cfg['class']
-        cfg['logger'] = logger
-        result = _aggregate_group(klass_name, payload, cfg)
-        logger.info("Aggregation result %s: %s", str(task['Meta']), str(result))
-        response.write(result)
-    except KeyError:
-        response.error(-100, "There's no class named %s" % klass_name)
-        logger.error("class %s is absent", klass_name)
-    except Exception as err:  # pylint: disable=broad-except
-        response.error(100, repr(err))
-        logger.error("Error %s", err)
-    finally:
-        response.close()
-
-
-def _aggregate_host(klass_name, payload, config, task):
-    available = plugin_import()
-    klass = available[klass_name]
-    handler = klass(config)
-    prevtime, currtime = task["PrevTime"], task["CurrTime"]
-    return handler.aggregate_host(payload, prevtime, currtime)
-
-
-def _aggregate_group(klass_name, payload, config):
-    available = plugin_import()
-    klass = available[klass_name]
-    handler = klass(config)
-    return handler.aggregate_group(payload)
+            logger.info("Aggregation result %s: %s", str(task['Meta']), str(result))
+            response.write(result)
+        except KeyError:
+            response.error(-100, "There's no class named %s" % klass_name)
+            logger.error("class %s is absent", klass_name)
+        except Exception as err:  # pylint: disable=broad-except
+            response.error(100, repr(err))
+            logger.error("Error %s", err)
+        finally:
+            response.close()
 
 
 if __name__ == '__main__':
+    C = Custom()
     W = Worker()
-    W.run({"aggregate_host": aggregate_host,
-           "aggregate_group": aggregate_group})
+    W.run({"aggregate_host": C.aggregate_host,
+           "aggregate_group": C.aggregate_group})
