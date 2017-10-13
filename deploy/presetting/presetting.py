47b0e63a6f81a9f9dd85769dfacf7c9b4f7a6271
Sergey Kacheev <s.kacheev@gmail.com>
2017-10-13 22:52:29 +0700

    deploy: move profile.json -> profile-default.json

--- deploy/presetting/presetting.py
+++ deploy/presetting/presetting.py
@@ -10,14 +10,20 @@ COMBAINE_RUNLIST = "combaine"
 try:
     s = Service("storage")
     try:
-        print("Create empty profile")
-        profile.Upload(s, "default", os.path.dirname(__file__) + "/profile.json").execute().get()
+        print("Update profile")
+        profile_json = os.path.dirname(__file__) + "/profile.json"
+        if not os.path.exists(profile_json):
+            print("Use default profile")
+            profile_json = os.path.dirname(__file__) + "/profile-default.json"
+        profile.Upload(s, "default", profile_json).execute().get()
     except ChokeEvent:
         pass
     if COMBAINE_RUNLIST not in runlist.List(s).execute().get():
         try:
             print("Create empty runlist")
-            runlist.Upload(s, COMBAINE_RUNLIST, os.path.dirname(__file__) + "/runlist.json").execute().get()
+            runlist.Upload(
+                s, COMBAINE_RUNLIST, os.path.dirname(__file__) + "/runlist.json"
+            ).execute().get()
         except ChokeEvent:
             pass
 except Exception as err:
