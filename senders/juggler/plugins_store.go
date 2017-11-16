afd40133ecb19fb32971d5658a3709b35070bb47
Sergey Kacheev <s.kacheev@gmail.com>
2017-11-17 01:22:12 +0700

    juggler: fix event storing

--- senders/juggler/plugins_store.go
+++ senders/juggler/plugins_store.go
@@ -4,7 +4,6 @@ import (
 	"github.com/combaine/combaine/common/logger"
 	"github.com/pkg/errors"
 	mgo "gopkg.in/mgo.v2"
-	"gopkg.in/mgo.v2/bson"
 )
 
 // juggler events history
@@ -29,6 +28,7 @@ type eventHistory struct {
 type pluginEventsStoreConfig struct {
 	Cluster  string `yaml:"cluster"`
 	Database string `yaml:"database"`
+	AuthDB   string `yaml:"authdb"`
 	User     string `yaml:"user"`
 	Password string `yaml:"password"`
 }
@@ -38,10 +38,25 @@ var eventsStore EventsStore
 // InitEventsStore initiate global plugin events store
 // for save juggler events history and use it as antiflap method
 func InitEventsStore(config *pluginEventsStoreConfig) {
-	eventsStore = &pluginEventsStore{config: config}
-	if err := eventsStore.Connect(); err != nil {
-		logger.Errf("Failed to connect event storage %s", err)
+	if config.Cluster == "" {
+		logger.Infof("Plugin events store not configured, skip")
+		return
 	}
+	if config.Database == "" {
+		config.Database = "combaine"
+		logger.Infof("Plugin event store use default db name 'combaine'")
+	}
+	if config.AuthDB == "" {
+		config.AuthDB = config.Database
+	}
+
+	store := &pluginEventsStore{config: config}
+	if err := store.Connect(); err != nil {
+		logger.Errf("Failed to connect event storage %s", err)
+		return
+	}
+	eventsStore = store
+	logger.Infof("Plugin event store connected sucessfully")
 }
 
 // Connect event storage
@@ -51,7 +66,7 @@ func (s *pluginEventsStore) Connect() error {
 	if err != nil {
 		return errors.Wrap(err, "mongo connect")
 	}
-	err = s.session.DB("admin").Login(s.config.User, s.config.Password)
+	err = s.session.DB(s.config.AuthDB).Login(s.config.User, s.config.Password)
 	if err != nil {
 		return errors.Wrap(err, "mongo login")
 	}
@@ -69,11 +84,18 @@ func (s *pluginEventsStore) Close() {
 func (s *pluginEventsStore) Push(key, event string, history int) ([]string, error) {
 	result := eventHistory{}
 	c := s.session.DB(s.config.Database).C("events")
-	err := c.Find(bson.M{"_id": key}).One(&result)
+	err := c.FindId(key).One(&result)
 	if err != nil {
-		return nil, errors.Wrap(err, "mongo findOne query")
+		result.Id = key
+		result.Events = []string{event}
+		err = c.Insert(result)
+		if err != nil {
+			return nil, errors.Wrap(err, "mongo insert query")
+		}
+		return result.Events, nil
 	}
 	if len(result.Events) < history {
+		result.Events = append(result.Events, "") // add room for one fresh
 		history = len(result.Events)
 	}
 	for i := history - 1; i > 0; i-- {
@@ -81,7 +103,7 @@ func (s *pluginEventsStore) Push(key, event string, history int) ([]string, erro
 	}
 	result.Events[0] = event
 	result.Events = result.Events[:history]
-	_, err = c.UpsertId(result.Id, result)
+	_, err = c.UpsertId(key, result)
 	if err != nil {
 		return nil, errors.Wrap(err, "mongo update query")
 	}
