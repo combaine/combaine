3a38dde69acb89d31f3ddbabea4232072077f550
Sergey Kacheev <s.kacheev@gmail.com>
2018-06-16 08:18:35 +0700

    deploy: fix deploy-cocaine-apps.sh

--- deploy/deploy-cocaine-apps.sh
+++ deploy/deploy-cocaine-apps.sh
@@ -3,15 +3,15 @@
 WD=/usr/lib/combaine/apps
 pushd $WD
 
+rm -vf ./*.tar.gz ./*.json
 for app in $(ls -1); do
     package=$app.tar.gz
     manifest=$app.json
 
-    rm -vf ./$package ./$manifest
     echo '{"slave": "'$app'"}' > $manifest
     tar -czf ./$package ./$app*
 
     cocaine-tool app upload --name $app --package=$package --manifest=$manifest
     cocaine-tool runlist add-app --name combaine --app $app --profile default
-
+    rm -vf ./$package ./$manifest
 done
