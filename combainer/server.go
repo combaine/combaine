0e6fb4b929a366e868a241d85f6e1947cf1c483c
Sergey Kacheev <s.kacheev@gmail.com>
2018-09-06 14:28:30 +0700

    server: create raft transport

--- combainer/server.go
+++ combainer/server.go
@@ -104,6 +104,9 @@ func (c *CombaineServer) Serve() error {
 		return errors.New("There are no combine operators here")
 	}
 
+	if err := c.cluster.createRaftTransport(); err != nil {
+		return err
+	}
 	c.cluster.joinSerf(hosts)
 
 	c.log.Info("start task distribution")
