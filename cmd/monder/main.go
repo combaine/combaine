6266ebac368461cdc011305bd5c65d60586cd508
Sergey Kacheev <s.kacheev@gmail.com>
2018-10-31 08:05:52 +0700

    senders: monder add "ts" tag

--- cmd/monder/main.go
+++ cmd/monder/main.go
@@ -37,7 +37,7 @@ func send(request *cocaine.Request, response *cocaine.Response) {
 	logger.Infof("%s Task len: %v documents save to '%s.%s'", task.ID, len(task.Data), cfg.Store.Database, task.Config.Namespace)
 	c := session.DB(cfg.Store.Database).C(task.Config.Namespace)
 	for _, d := range task.Data {
-		d.Tags["ts"] = strconv.FormatInt(time.Now().Unix(), 64)
+		d.Tags["ts"] = strconv.FormatInt(time.Now().Unix(), 10)
 		if err = c.Insert(d); err != nil {
 			// https://godoc.org/gopkg.in/mgo.v2#Session.Refresh
 			session.Refresh()
