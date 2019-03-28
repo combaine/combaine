7bf5e6a5131cb3ce5b9d913f1d0eaf526d9e4e35
Sergey Kacheev <skacheev@yandex-team.ru>
2019-03-28 12:02:27 +0700

    fsm: test keys

--- combainer/serf_test.go
+++ combainer/serf_test.go
@@ -1,1 +1,23 @@
 package combainer
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestKeysUtil(t *testing.T) {
+	testMap := make(map[string]map[string]chan struct{})
+	testMap["one"] = map[string]chan struct{}{
+		"key1": make(chan struct{}),
+		"key2": make(chan struct{}),
+		"key3": make(chan struct{}),
+	}
+
+	assert.EqualValues(t, []string{"key1", "key2", "key3"}, keys(testMap["one"]))
+
+	testMap["two"] = map[string]chan struct{}{
+		"key": make(chan struct{}),
+	}
+	assert.EqualValues(t, []string{"key"}, keys(testMap["two"]))
+}
