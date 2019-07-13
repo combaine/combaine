c0fde6ad1d122fa43761423e7fbaf09a9947d450
Sergey Kacheev <skacheev@yandex-team.ru>
2019-07-13 23:39:27 +0700

    worker: remove useless tests

--- worker/aggregating_test.go
+++ worker/aggregating_test.go
@@ -1,15 +1,11 @@
 package worker
 
 import (
-	"context"
-	"fmt"
 	"testing"
 
 	"github.com/stretchr/testify/assert"
 
 	"github.com/combaine/combaine/repository"
-	"github.com/combaine/combaine/utils"
-	"github.com/sirupsen/logrus"
 )
 
 const cfgName = "aggCore"
@@ -34,69 +30,4 @@ func TestInit(t *testing.T) {
 }
 
 func TestAggregating(t *testing.T) {
-	logrus.SetLevel(logrus.DebugLevel)
-
-	hostsPerDc := map[string][]string{
-		"DC1": {"Host1", "Host2"},
-		"DC2": {"Host3", "Host4"},
-	}
-
-	encHosts, _ := utils.Pack(hostsPerDc)
-	encParsingConfig, _ := utils.Pack(parsingConfig)
-	encAggregationConfig, _ := utils.Pack(aggregationConfig)
-
-	aggTask := AggregatingTask{
-		Id:                       "testId",
-		Frame:                    &TimeFrame{Current: 61, Previous: 1},
-		Config:                   cfgName,
-		ParsingConfigName:        cfgName,
-		EncodedParsingConfig:     encParsingConfig,
-		EncodedAggregationConfig: encAggregationConfig,
-		EncodedHosts:             encHosts,
-		ParsingResult: &ParsingResult{
-			Data: map[string][]byte{
-				"Host1;appsName": []byte("Host1;appsName"),
-				"Host2;appsName": []byte("Host2;appsName"),
-				"Host3;appsName": []byte("Host3;appsName"),
-				"Host4;appsName": []byte("Host4;appsName"),
-			},
-		},
-	}
-
-	go func() {
-
-		expectAggregatingGroup := map[string]bool{
-			"Host1":                false,
-			"Host2":                false,
-			"Host3":                false,
-			"Host4":                false,
-			"Host1Host2":           false,
-			"Host3Host4":           false,
-			"Host1Host2Host3Host4": false,
-		}
-		sendersCount := map[string]int{
-			"Host1":          0,
-			"Host2":          0,
-			"Host3":          0,
-			"Host4":          0,
-			"DC1":            0,
-			"DC2":            0,
-			"test-combainer": 0, // metahost
-		}
-
-		//shouldSendToSenders := len(aggregationConfig.Senders)
-
-		t.Log("Test aggregators")
-		for k, v := range expectAggregatingGroup {
-			assert.True(t, v, fmt.Sprintf("aggregating for %s failed", k))
-		}
-		t.Log("Test senders")
-		for k, v := range sendersCount {
-			assert.Equal(t, len(aggregationConfig.Senders), v, fmt.Sprintf("senders for '%s' failed", k))
-		}
-	}()
-	assert.NoError(t, DoAggregating(context.TODO(), &aggTask))
-}
-
-func TestEnqueue(t *testing.T) {
 }
